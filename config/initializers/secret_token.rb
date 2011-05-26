d874813698ddff747c62d4115b0b88ccf1906860
Ilya Zhitomirskiy <ilya@laptop.(none)>
2011-05-26 00:20:19 -0700

    WIP generated over the app, fixed up the names, actually have to setup postgres

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SignInWithTwitter::Application.config.secret_token = 'ff2d6c0d9edd8f7cedc2d2df25710f08ab212971fd3bcdefbfa73ab1b002f4096a7be6fdba5445d83f59212d093faa053c61ecefc5e26749bbb292e7466d705f'
+Ciccflip::Application.config.secret_token = '4f0a91f238db7ad65d43c48e217cd1e344664b729c6fe2ee49fd408e714709597170e8b63eb26bcc59105ca93a564e8d6d012fd9a74570ce18653a70be7bcbf4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SignInWithTwitter::Application.routes.draw do
+Ciccflip::Application.routes.draw do
   root :to => 'welcome#index'
   get '/auth/twitter/callback', :to => 'sessions#create', :as => 'callback'
   get '/auth/failure', :to => 'sessions#error', :as => 'failure'
